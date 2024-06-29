a3a1aa2330e79da4ef7c80e5b727b6e521c5cb6d
Gaëtan Lovey <gaetan.lovey@gmail.com>
2024-06-29 11:32:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-st.title('Affichage de DataFrame avec Streamlit')
+st.title('Interesting games')
 
-st.write("Voici un exemple de DataFrame :")
+st.write("Bookmaker above average :")
 df = pd.read_excel('df.xlsx')
 st.dataframe(df)  # Vous pouvez utiliser st.table(df) pour un affichage statique
