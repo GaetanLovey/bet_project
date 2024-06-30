5108e5d4888231fe341b3feef75023989d51a81c
Gaëtan Lovey <gaetan.lovey@gmail.com>
2024-06-30 16:05:52 +0200

    test

--- app.py
+++ app.py
@@ -180,25 +180,22 @@ def success_page():
 
     # Afficher un message de succès ou rediriger l'utilisateur
     st.success('Payment successful! Redirecting to the main page...')
-    
+
     # Bouton Log Out
     if st.button('Log Out'):
         st.session_state['authenticated'] = False
         st.session_state['username'] = None
         st.experimental_rerun()
 
-    # Rediriger vers la page principale après un court délai
-    st.experimental_set_query_params()
-    st.experimental_rerun()
-
-
 # Gestion des états de l'application
 if 'authenticated' not in st.session_state:
     st.session_state['authenticated'] = False
     st.session_state['username'] = None
 
-# Afficher la bonne page en fonction de l'état d'authentification
-if st.session_state['authenticated']:
+# Vérifier si l'URL contient le paramètre de succès après le paiement
+if st.experimental_get_query_params().get('payment-success'):
+    success_page()  # Afficher la page de succès de paiement
+elif st.session_state['authenticated']:
     main_page(st.session_state['username'])  # Afficher la page principale si l'utilisateur est authentifié
 else:
     st.write('Welcome to Bet Project')
@@ -209,7 +206,3 @@ else:
         signup_page()  # Afficher la page de création de compte
     elif option == 'Login':
         login_page()  # Afficher la page de login
-
-# Vérifier si l'URL contient le paramètre de succès après le paiement
-if st.experimental_get_query_params().get('payment-success'):
-    success_page()  # Afficher la page de succès de paiement
